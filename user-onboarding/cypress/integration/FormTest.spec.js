d9a1f3f5ff77ebe092ddfb29209b358dca42a519
Chris Forrest <redrider_cr@yahoo.com>
2020-04-23 18:23:34 -0400

    made some varialbes in the FormTest.spec.js

--- user-onboarding/cypress/integration/FormTest.spec.js
+++ user-onboarding/cypress/integration/FormTest.spec.js
@@ -1,3 +1,8 @@
+import {v4 as uuid } from 'uuid'
+const username = uuid().slice(0, 5)
+const email = `${username}@acme.com`
+const password = uuid().slice(0, 7)
+
 it('It works!!!', ()=> {
     expect (5).to.eq(5)
 })
