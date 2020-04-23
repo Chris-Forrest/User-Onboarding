801b337c3d38fd0b4a65d7140adbd3217d6c80de
Chris Forrest <redrider_cr@yahoo.com>
2020-04-23 09:40:52 -0400

    I had to start a new repository i deleted some nodes when i messed up a commit and messed up my github trying to fix the mistake but i saved my code in an office document

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,169 @@
+import React, { useState, useEffect } from "react";
+import * as yup from "yup";
+import axios from "axios";
+import styled from "styled-components";
+import Form from "./components/Form";
+import "./App.css";
+
+const StyledContainer = styled.div`
+display:flex;
+flex-direction:column;
+align-items:center;
+border:1px solid rgb(210, 210, 210 );
+border-radius: 5px;
+box-shadow: 10px 8px 12px -2px rgb(128, 127, 197);
+margin: 8px;
+padding: 12px;
+background-color:#39aafa;
+width: 50%;
+`
+
+const url = "https://reqres.in/api/users"
+
+
+
+const initialFormValues = {
+  username: "",
+  email: "",
+  password: "",
+  termsOfService: false,
+};
+
+const initialFormErrors = {
+  username: "",
+  email: "",
+  password: "",
+  termsOfService: false,
+};
+
+const formValidation = yup.object().shape({
+  username: yup
+    .string()
+    .min(3, "username must have at least 3 characters!")
+    .required("username is required!"),
+  email: yup
+    .string()
+    .email("a VALID email is required")
+    .required("email is required"),
+  password: yup
+    .string()
+    .required("password is required"),
+   
+
+})
+
+export default function App() {
+  const [users, setUsers] = useState([])
+
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+  const [formDisabled, setFormDisabled] = useState(true)
+
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+
+  // const getUsers = () => {
+  //   axios
+  //     .get(url)
+  //     .then((res) => {
+  //       setUsers(res.data.data);
+  //     })
+  //     .catch((err) => {
+  //       debugger;
+  //     })
+  // }
+
+  // useEffect(() => {
+  //   getUsers();
+  // }, []);
+  // console.log(users)
+
+  const postUser = (user) => {
+    axios
+      .post(url, user)
+      .then((res) => {
+        setUsers([...users, res.data])
+        console.log(res.data);
+      })
+      .catch((err) => {
+        debugger;
+      })
+  }
+
+useEffect(()=> {
+
+}, [users])
+
+  const changeValues = (event) => {
+
+    const name = event.target.name
+    const value = event.target.value
+    yup
+    .reach(formValidation, name)
+    .validate(value)
+    .then(valid => {
+      setFormErrors({
+        ...formErrors,
+        [name]: '',
+      })
+    })
+    .catch(err => {
+      setFormErrors({
+        ...formErrors,
+        [name]: err.errors[0]
+      })
+    })
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    })
+  }
+
+  const checkboxChange = (event) => {
+    setFormValues({
+      ...formValues,
+      [event.target.name]: event.target.checked,
+    })
+  }
+
+  useEffect(()=>{
+    formValidation.isValid(formValues)
+    .then(valid => {
+      setFormDisabled(!valid)
+    })
+  }, [formValues])
+
+  const submitUser = (event) => {
+    event.preventDefault()
+
+    const newUser = {
+      username: formValues.username,
+      email: formValues.email,
+      password: formValues.password,
+      termsOfService: formValues.termsOfService,
+    };
+// console.log(newUser)
+    postUser(newUser)
+    setFormValues(initialFormValues)
+  };
+
+  return (
+    <StyledContainer>
+      <Form
+        values={formValues}
+        changeValues={changeValues}
+        checkboxChange={checkboxChange}
+        submitUser={submitUser}
+        disabled={formDisabled}
+        errors={formErrors}
+      />
+      {
+        users.map(user=>{
+         return( <div className="userCard">
+            <h2>{user.username}</h2>
+        <p>{user.email}</p>
+          </div>)
+        })
+      }
+    </StyledContainer>
+  );
+}
---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,79 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledForm = styled.form`
+display:flex;
+flex-direction:column;
+align-items:center;
+border:1px solid rgb(210, 210, 210 );
+border-radius: 5px;
+box-shadow: 10px 8px 12px -2px rgb(128, 127, 197);
+margin: 8px;
+padding: 12px;
+background-color:white;
+width: 50%;
+
+`;
+
+const Warning = styled.p`
+    width: 100%;
+    font-size: 1.5rem;
+    color: red;
+    box-shadow: none;
+    margin: 1% 0;
+`
+
+const StyledLabel =styled.label`
+    /* color: white; */
+`
+
+export default function Form({
+  values,
+  changeValues,
+  checkboxChange,
+  submitUser,
+  disabled,
+  errors,
+}) {
+  return (
+    <StyledForm onSubmit={submitUser}>
+      <div className="errors">
+        <Warning>
+            {errors.username}
+        </Warning>
+        <Warning>
+            {errors.email}
+        </Warning>
+        <Warning>
+            {errors.password}
+        </Warning>
+        <Warning>
+            {errors.terms}
+        </Warning>
+      </div>
+      <StyledLabel>User Name</StyledLabel>
+      <input
+        name="username"
+        value={values.username}
+        onChange={changeValues}
+      ></input>
+      <StyledLabel>Email</StyledLabel>
+      <input name="email" value={values.email} onChange={changeValues}></input>
+      <StyledLabel>Password</StyledLabel>
+      <input
+        name="password"
+        value={values.password}
+        onChange={changeValues}
+      ></input>
+      <StyledLabel>Terms of Service</StyledLabel>
+      <input
+        type="checkbox"
+        name="termsOfService"
+        onChange={checkboxChange}
+      ></input>
+      <button onClick={submitUser} disabled={disabled}>
+        Submit
+      </button>
+    </StyledForm>
+  );
+}
